d5c9489ad48ba1deeed126d306db20124b290a3f
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2022-01-09 20:35:24 +0530

    Create task2a.cpp

---
+++ task2a.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Base{
+    public:
+    ~Base()
+    {
+        cout<<"Base class destructor\n";
+        cout<<endl;
+    }
+};
+class Derived : public Base{
+    public:
+    ~Derived()
+    {
+        cout<<"Derived class destructor\n";
+        cout<<endl;
+    }
+};
+int main() 
+{
+    Base *ptr = new Derived;  
+    delete ptr;   
+}
